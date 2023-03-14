cbb8ebc07de76b8f916f158cd91404bef832946a
sunking-1996 <15167168967@163.com>
2023-03-14 17:51:06 +0800

    20230314 update

--- test.py
+++ test.py
@@ -7,11 +7,11 @@ urllib3.disable_warnings()  # 忽略警告
 def test_login():
     '''用例描述: 登录接口，正确入参'''
     url = "https://www.fhd001.com/loginAccount.do"
-    parm = {
+    body = {
     "userAccount": "15167168967",
     "userPwd": "123456"
     }
-    r = requests.get(url, params=parm,verify=False)
+    r = requests.post(url, data=body,verify=False)
     print("\n"+"返回的结果:%s" % r.text)
     # 实际结果
     result_rcode = r.json().get("rcode")
